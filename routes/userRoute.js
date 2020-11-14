315f0f78150ba8884043ff624b26ddc156948082
Topi Paunola <Topi.Paunola@metropolia.fi>
2020-11-15 00:52:13 +0200

    val

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -11,9 +11,10 @@ router.get('/id:', userController.user_get);
 
 
 router.post('/', [
-    body('name', 'vähintään 3 merkkiä').isLength({min: 3}),
+    body('name', 'vähintään 3 merkkiä').isLength({min: 3}).escape(),
     body('email', 'kunnon sähköposti').isEmail(),
-    body('passwd', 'vähintään 8 merkkiä ja sisältää isoja kirjaimia').matches('(?=.*[A-Z]).{8,}')
+    body('passwd', 'vähintään 8 merkkiä ja sisältää isoja kirjaimia').matches(
+        '(?=.*[A-Z]).{8,}'),
 ],userController.user_create_post);
 
 router.put('/', (req, res) => {
