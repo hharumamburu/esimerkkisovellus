2f456e0f82c38b5cba985d95d9779c1215dbf8f3
Topi Paunola <Topi.Paunola@metropolia.fi>
2020-11-15 20:44:19 +0200

    week4

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const catRoute = require('./routes/catRoute');
 const userRoute = require('./routes/userRoute');
+const passport = require('./utils/pass.js');
+const authRoute = require('./routes/authRoute.js');
 const app = express();
 const port = 3000;
 
@@ -14,8 +16,9 @@ app.use(express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use('/cat', catRoute);
-app.use('/user', userRoute);
+app.use('/auth', authRoute);
+app.use('/cat', passport.authenticate('jwt', {session: false}), catRoute);
+app.use('/user', passport.authenticate('jwt', {session: false}), userRoute);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
